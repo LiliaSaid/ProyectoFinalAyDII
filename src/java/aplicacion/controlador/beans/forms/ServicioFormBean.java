53724a5776284ceb8f273d27ccf66bd19156589e
LILI-PC <yazminsaid@gmail.com>
2019-06-28 12:46:33 -0300

    Se muestra un select con los servicios encontrados

--- src/java/aplicacion/controlador/beans/forms/ServicioFormBean.java
+++ src/java/aplicacion/controlador/beans/forms/ServicioFormBean.java
@@ -1,8 +1,10 @@
-
 package aplicacion.controlador.beans.forms;
 
 import aplicacion.controlador.beans.ServicioBean;
+import aplicacion.modelo.dominio.Servicio;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -16,5 +18,35 @@ public class ServicioFormBean implements Serializable {
 
     private String servicio;
 
+    public ServicioFormBean() {
+    }
+
+    public List<String> autoCompletar(String query) {
+        List<String> results = new ArrayList<String>();
+
+        for (Servicio unServicio : servicioBean.getServicioList()) {
+            if (query.contains(query)) {
+                results.add(unServicio.getNombre());
+            }
+        }
+
+        return results;
+    }
+
+    public ServicioBean getServicioBean() {
+        return servicioBean;
+    }
+
+    public void setServicioBean(ServicioBean servicioBean) {
+        this.servicioBean = servicioBean;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
 
 }
