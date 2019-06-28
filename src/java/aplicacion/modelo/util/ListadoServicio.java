462e238b810d402a5cc76d91809f780599b5a7fd
LILI-PC <yazminsaid@gmail.com>
2019-06-28 17:04:39 -0300

    actualizo listado de servicios

--- src/java/aplicacion/modelo/util/ListadoServicio.java
+++ src/java/aplicacion/modelo/util/ListadoServicio.java
@@ -18,9 +18,18 @@ public class ListadoServicio {
 
     public ListadoServicio() {
         listaServicios = new ArrayList<>();
-        listaServicios.add(new Servicio(1, "Energia", "Ejesa"));
-        listaServicios.add(new Servicio(2, "Energia", "La lo"));
-        listaServicios.add(new Servicio(3, "Energia", "La le"));
+        
+        listaServicios.add(new Servicio(218003, "Luz y Agua",               "Ejesa"));
+        listaServicios.add(new Servicio(218033, "Luz y Agua",               "Agua de los Andes"));
+        listaServicios.add(new Servicio(418004, "Telefonia",                "Telecom"));
+        listaServicios.add(new Servicio(418014, "Telefonia",                "Movistar"));
+        listaServicios.add(new Servicio(418024, "Telefonia",                "Personal"));
+        listaServicios.add(new Servicio(418034, "Telefonia",                "Claro"));
+        listaServicios.add(new Servicio(718059, "Proveedores de Internet",  "Wirenet"));
+        listaServicios.add(new Servicio(718069, "Proveedores de Internet",  "Telecentro"));
+        listaServicios.add(new Servicio(170009, "Operadoras de TV",         "Canal 2"));
+        listaServicios.add(new Servicio(170019, "Operadoras de TV",         "Canal 4"));
+        
     }    
     
     public void agregar(Servicio servicio){
