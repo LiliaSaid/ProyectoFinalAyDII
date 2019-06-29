b89ebd11650cded380c43735d5519e09fd9615cb
LILI-PC <yazminsaid@gmail.com>
2019-06-29 14:59:38 -0300

    agrego atributos de la tarjeta al checkoutForm

--- src/java/aplicacion/controlador/beans/forms/CheckoutFormBean.java
+++ src/java/aplicacion/controlador/beans/forms/CheckoutFormBean.java
@@ -1,11 +1,11 @@
 package aplicacion.controlador.beans.forms;
 
 import aplicacion.controlador.beans.FacturaBean;
-import aplicacion.controlador.beans.ServicioBean;
 import aplicacion.modelo.dominio.Factura;
 import aplicacion.modelo.dominio.Usuario;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -22,7 +22,13 @@ public class CheckoutFormBean implements Serializable {
 
     private List<Factura> facturas;
     private double subTotal;
-    
+
+    private String marcaTarjeta;
+    private String numeroTarjeta;
+    private String titularTarjeta;
+    private String vencimientoTarjeta;
+    private int codigoTarjeta;
+
     public CheckoutFormBean() {
         Usuario usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
         if (usuario == null) {
@@ -37,12 +43,80 @@ public class CheckoutFormBean implements Serializable {
     public void init() {
         facturas = (List<Factura>) FacesContext.getCurrentInstance().getExternalContext()
                 .getSessionMap().get("facturasAPagar");
-        
+
         facturas.stream().forEach((unaFactura) -> {
             subTotal += unaFactura.getImporte();
-        });        
-        
+        });
+
     }
 
- 
+    public String pagarFacturas() {
+
+        return "";
+    }
+
+    public FacturaBean getFacturaBean() {
+        return facturaBean;
+    }
+
+    public void setFacturaBean(FacturaBean facturaBean) {
+        this.facturaBean = facturaBean;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public String getMarcaTarjeta() {
+        return marcaTarjeta;
+    }
+
+    public void setMarcaTarjeta(String marcaTarjeta) {
+        this.marcaTarjeta = marcaTarjeta;
+    }
+
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public String getTitularTarjeta() {
+        return titularTarjeta;
+    }
+
+    public void setTitularTarjeta(String titularTarjeta) {
+        this.titularTarjeta = titularTarjeta;
+    }
+
+    public String getVencimientoTarjeta() {
+        return vencimientoTarjeta;
+    }
+
+    public void setVencimientoTarjeta(String vencimientoTarjeta) {
+        this.vencimientoTarjeta = vencimientoTarjeta;
+    }
+
+    public int getCodigoTarjeta() {
+        return codigoTarjeta;
+    }
+
+    public void setCodigoTarjeta(int codigoTarjeta) {
+        this.codigoTarjeta = codigoTarjeta;
+    }
+
 }
